85fff837a35ae18f484f2261a47194bf38d5b171
Thoriqsulthann <118926068+Thoriqsulthann@users.noreply.github.com>
2023-01-08 23:33:23 +0700

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+   public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+                ;
+            }
+    } 
+}
